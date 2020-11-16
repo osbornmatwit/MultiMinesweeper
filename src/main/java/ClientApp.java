9efd7a613e490ea839c0d2008700b6fe83b8ecb8
hpadilla6201 <hpadilla6201@gmail.com>
2020-11-16 14:12:43 -0500

    configuring stage

--- src/main/java/ClientApp.java
+++ src/main/java/ClientApp.java
@@ -1,21 +1,23 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.scene.layout.Pane;
 public class ClientApp extends Application {
+
+    private Parent createContent() {
+        Pane root = new Pane();
+        root.setPrefSize(800, 600);
+
+        return root;
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+    Scene scene = new Scene(createContent());
 
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
-
-        stage.setTitle("JavaFX and Gradle");
-        stage.setScene(scene);
-        stage.show();
+    stage.setScene(scene);
+    stage.show();
     }
 
     public static void main(String[] args) {
